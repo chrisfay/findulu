7e6b47c95d62d4c7de32ad978dc182b2fe61dff3
Chris Fay, the Overloard <chrisfay@chrisfay.net>
2009-10-14 12:14:19 -0500

    added fields and query for search results page

--- application/views/front_end/search_results.php
+++ application/views/front_end/search_results.php
@@ -10,10 +10,21 @@ if(! is_null($content['search_results']))
 	echo '<h3>Results:</h3>';
 	foreach($content['search_results']->result() as $row) 
 	{
-		echo '<strong>Title</strong>: '. $row->title . "<br />";
-		echo print_r($row) . "<br /><br />";		
+		//echo print_r($row);?>
+		<ul class="listing_result">
+			<li><a href="<?php echo base_url() . 'view_single_listing/' .$row->listing_id?>"><?php echo $row->title ?></a></li>
+			<li>Phone: <?php echo $row->phone ?></li>
+			<li>Email: <?php echo $row->email ?></li>
+			<li>zip: <?php echo $row->zip ?></li>
+			<li>listing_ad_filename: <?php echo $row->listing_ad_filename ?></li>
+			<li>listing_url: <?php echo $row->listing_url ?></li>
+			<li>city: <?php echo $row->city ?></li>
+			<li>state: <?php echo $row->state_name ?></li>
+		</ul>		
+<?
 	}
 }
+?>
 		
 
 
